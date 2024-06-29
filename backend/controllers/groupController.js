06fb596caad4abc7cb064f0b4021564c55dcc4d2
Ankur Singh <ankursingh9806@gmail.com>
2024-06-29 14:21:50 +0530

    function - getGroups, createGroup

---
+++ backend/controllers/groupController.js
@@ -0,0 +1,33 @@
+const User = require("../models/userModel");
+const Group = require("../models/groupModel");
+const UserGroup = require("../models/userGroupModel");
+
+const getGroups = async (req, res) => {
+    try {
+        const groups = await Group.findAll();
+        res.status(200).json({ message: "group fetched", groups: groups });
+    } catch (err) {
+        console.error("error:", err);
+        res.status(500).json({ error: "internal server error" });
+    }
+};
+
+const createGroup = async (req, res) => {
+    try {
+        const { groupName } = req.body;
+        const group = await Group.create({ name: groupName });
+        await UserGroup.create({
+            UserId: req.user.id,
+            groupId: group.id,
+        });
+        res.status(201).json({ message: "group created", group });
+    } catch (error) {
+        console.error('error:', error);
+        res.status(500).json({ error: 'failed to create group' });
+    }
+};
+
+module.exports = {
+    getGroups,
+    createGroup,
+}
