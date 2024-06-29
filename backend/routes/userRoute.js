838633a6c6203d7a6fea24e3bde4295cb20c1aba
Ankur Singh <ankursingh9806@gmail.com>
2024-06-29 09:51:02 +0530

    route - /signup-page, /login-page, /signup, /login

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const userController = require("../controllers/userController");
+
+router.get("/signup-page", userController.signupPage);
+router.get("/login-page", userController.loginPage);
+router.post("/signup", userController.signup);
+router.post("/login", userController.login);
+
+module.exports = router;
