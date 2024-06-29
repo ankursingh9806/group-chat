8da5a14d52fb577e989885afafcfec1b089bac9d
Ankur Singh <ankursingh9806@gmail.com>
2024-06-29 14:22:55 +0530

    function - sendMessage, getMessage

---
+++ backend/controllers/messageController.js
@@ -0,0 +1,36 @@
+const Message = require("../models/messageModel");
+const Group = require("../models/groupModel");
+
+const sendMessage = async (req, res) => {
+    try {
+        const { message, groupId } = req.body;
+        const newMessage = await Message.create({
+            name: req.user.name,
+            message,
+            UserId: req.user.id,
+            groupId
+        });
+        res.status(201).json({ message: "message sent", data: newMessage });
+    } catch (err) {
+        console.error("error:", err);
+        res.status(500).json({ error: "internal server error" });
+    }
+};
+
+const getMessage = async (req, res) => {
+    try {
+        const { groupId } = req.params;
+        const messages = await Message.findAll({
+            where: { groupId: groupId },
+        });
+        res.status(200).json({ message: "message received", data: messages });
+    } catch (err) {
+        console.error("error:", err);
+        res.status(500).json({ error: "internal server error" });
+    }
+};
+
+module.exports = {
+    sendMessage,
+    getMessage,
+};
