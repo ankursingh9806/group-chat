aaa5f542d3b071c0b15d96ded9dda46524787403
Ankur Singh <ankursingh9806@gmail.com>
2024-06-30 19:46:55 +0530

    route - /delete-group/:groupId

--- backend/routes/groupRoute.js
+++ backend/routes/groupRoute.js
@@ -6,5 +6,6 @@ const authentication = require("../middleware/authentication");
 
 router.get("/get-groups", authentication.authenticate, groupController.getGroups);
 router.post("/create-group", authentication.authenticate, groupController.createGroup);
+router.delete("/delete-group/:groupId", authentication.authenticate, groupController.deleteGroup);
 
 module.exports = router;
