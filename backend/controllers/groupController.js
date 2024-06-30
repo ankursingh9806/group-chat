03c98ea67449c73e170b4ec73f577a495ecc9cb4
Ankur Singh <ankursingh9806@gmail.com>
2024-06-30 19:45:12 +0530

    add admin in Group model; function - deleteGroup

--- backend/controllers/groupController.js
+++ backend/controllers/groupController.js
@@ -15,19 +15,38 @@ const getGroups = async (req, res) => {
 const createGroup = async (req, res) => {
     try {
         const { groupName } = req.body;
-        const group = await Group.create({ name: groupName });
+        const group = await Group.create({
+            name: groupName,
+            admin: req.user.id,
+        });
         await UserGroup.create({
             UserId: req.user.id,
             groupId: group.id,
         });
-        res.status(201).json({ message: "group created", group });
+        res.status(201).json({ message: "group created", group: group });
     } catch (error) {
         console.error("error:", error);
         res.status(500).json({ error: "failed to create group" });
     }
 };
 
+const deleteGroup = async (req, res) => {
+    try {
+        const { groupId } = req.params;
+        const group = await Group.findByPk(groupId);
+        if (group.admin !== req.user.id) {
+            return res.status(403).json({ error: "only admin can delete this group" });
+        }
+        await group.destroy();
+        res.status(200).json({ message: "group deleted" });
+    } catch (err) {
+        console.error("error:", err);
+        res.status(500).json({ error: "internal server error" });
+    }
+};
+
 module.exports = {
     getGroups,
     createGroup,
+    deleteGroup,
 }
