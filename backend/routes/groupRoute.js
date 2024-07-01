d0f6e312cd60120eaa4755c3208d7334b7bae4c7
Ankur Singh <ankursingh9806@gmail.com>
2024-07-01 08:51:16 +0530

    route - /add-to-group, /remove-from-group

--- backend/routes/groupRoute.js
+++ backend/routes/groupRoute.js
@@ -7,5 +7,7 @@ const authentication = require("../middleware/authentication");
 router.get("/get-groups", authentication.authenticate, groupController.getGroups);
 router.post("/create-group", authentication.authenticate, groupController.createGroup);
 router.delete("/delete-group/:groupId", authentication.authenticate, groupController.deleteGroup);
+router.post("/add-to-group", authentication.authenticate, groupController.addToGroup);
+router.post("/remove-from-group", authentication.authenticate, groupController.removeFromGroup);
 
 module.exports = router;
