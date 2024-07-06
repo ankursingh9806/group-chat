3d6bee459fefd1ded041f5d4f7fe17534a202013
Ankur Singh <ankursingh9806@gmail.com>
2024-07-06 18:32:13 +0530

    route - /upload-file/:groupId; multer

--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const messageController = require("../controllers/messageController");
 const authentication = require("../middleware/authentication");
+const upload = require("../utils/multer");
 
 router.post("/send-message", authentication.authenticate, messageController.sendMessage);
 router.get("/get-message/:groupId", authentication.authenticate, messageController.getMessage);
+router.post("/upload-file/:groupId", authentication.authenticate, upload.single("file"), messageController.uploadFile);
 
 module.exports = router;
