ff84bf39559df5598ed307744efd2e447d7b8765
Ankur Singh <ankursingh9806@gmail.com>
2024-07-06 21:24:38 +0530

    ArchiveChat model - id, name, message, fileUrl, UserId, groupId

---
+++ backend/models/archiveChatModel.js
@@ -0,0 +1,32 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../utils/database");
+
+const ArchiveChat = sequelize.define("ArchiveChat", {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    name: {
+        type: Sequelize.STRING,
+    },
+    message: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+    fileUrl: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+    UserId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    groupId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+});
+
+module.exports = ArchiveChat;
