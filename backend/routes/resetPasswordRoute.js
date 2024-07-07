37d6542dcb969261a66a8e76b6c6e8ad30fccbf3
Ankur Singh <ankursingh9806@gmail.com>
2024-07-07 20:02:36 +0530

    routes - /forgot-password-page, /forgot-password, /reset-password-page/:requestId, /reset-password/:resetId

---
+++ backend/routes/resetPasswordRoute.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const resetPasswordController = require("../controllers/resetPasswordController");
+
+router.get("/forgot-password-page", resetPasswordController.forgotPasswordPage);
+router.post("/forgot-password", resetPasswordController.forgotPassword);
+router.get("/reset-password-page/:requestId", resetPasswordController.resetPasswordPage);
+router.post("/reset-password/:resetId", resetPasswordController.resetPassword);
+
+module.exports = router;
