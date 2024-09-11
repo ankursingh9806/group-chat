06c216a9ba38da9468e022b250e145408899b19d
Ankur Singh <ankursingh9806@gmail.com>
2024-09-11 19:24:11 +0530

    reset password model

--- backend/models/resetPasswordModel.js
+++ backend/models/resetPasswordModel.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 const sequelize = require("../utils/database");
 
-const ResetPassword = sequelize.define("ResetpPssword", {
+const ResetPassword = sequelize.define("ResetPassword", {
     id: {
         type: Sequelize.UUID,
         defaultValue: Sequelize.UUIDV4,
